f746e9591ff0e12ae372f04c5254dcb5f35e74be
helioribeiro <helioribeiropro@gmail.com>
2026-07-13 13:40:00 +0200

    test(commit): commit with correct logic

--- sum.py
+++ sum.py
@@ -3,9 +3,9 @@ def add(a, b):
 
 
 def calculate_result():
-    return add(2, 4)
+    return add(2, 2)
 
 
 if __name__ == "__main__":
     result = calculate_result()
-    print(f"2 + 4 = {result}")
+    print(f"2 + 2 = {result}")
